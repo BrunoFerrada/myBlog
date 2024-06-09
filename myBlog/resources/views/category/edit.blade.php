8b85ef9451fd0f9a624dd1111e96b3511e8a6d34
BrunoFerrada <brunoferrada1212@gmail.com>
2024-06-09 05:45:59 -0300

    style added to edit

--- myBlog/resources/views/category/edit.blade.php
+++ myBlog/resources/views/category/edit.blade.php
@@ -1,20 +1,26 @@
 <x-app-layout>
-    <div>
+    <div class="bg-gray-800 rounded-lg w-96 p-6 mx-auto my-8">
         <h1 class="text-gray-100 text-center">Editar post</h1>
-        <form action="/category/show/{{$post->id}}" method="POST">
+        <form action="/category/show/{{$post->id}}" method="POST" class="space-y-4 mx-auto">
 
             @csrf
             @method("PUT")
-            <label>
-                <input type="text" name="title" value="{{$post->title}}" placeholder="Ingrese un título">
-            </label>
+            <div>
+                <label>
+                    <input type="text" name="title" value="{{$post->title}}" placeholder="Ingrese un título" class="w-full px-3 py-2 rounded-md border bg-gray-900 border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 text-gray-100">
+                </label>
+            </div>
 
-            <label>
-                <textarea name="content" placeholder="Ingrese su contenido">{{$post->content}}</textarea>
-            </label>
+            <div>
+                <label>
+                    <textarea name="content" placeholder="Ingrese su contenido" class="w-full px-3 py-2 rounded-md border bg-gray-900 border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 text-gray-100">{{$post->content}}</textarea>
+                </label>
+            </div>
 
-            <button type="submit"> Actualizar post </button>
-            <a href="/category/"> Volver </a>
+            <div class="flex justify-between items-center">
+            <button type="submit" class="bg-blue-500 text-gray-100 px-4 py-2 rounded-md hover:bg-blue-600"> Actualizar post </button>
+            <a href="/category/" class="bg-blue-500 text-gray-100 px-4 py-2 rounded-md hover:bg-blue-600"> Volver </a>
+            </div>
         </form>
     </div>
 </x-app-layout>
--- myBlog/resources/views/category/show.blade.php
+++ myBlog/resources/views/category/show.blade.php
@@ -7,16 +7,16 @@
         <p class="text-gray-100 mb-4">
             {{$post->content}}
         </p>
-
-    @if (Auth::check() && Auth::user()->name == $post->poster)
-    <a href="/category/edit/{{$post->id}}" class="bg-blue-500 rounded-md  px-4 py-2 text-gray-100 hover:bg-blue-600">editar</a>
-    <form action="/category/show/{{$post->id}}" method="POST" >
-        @csrf
-        @method("DELETE")
-        <button type="submit" > Eliminar post </button>
-    </form>
-@endif
-
-    
+        <div class="flex justify-between">
+            @if (Auth::check() && Auth::user()->name == $post->poster)
+                <a href="/category/edit/{{$post->id}}" class="bg-blue-500 rounded-md  px-4 py-2 text-gray-100 hover:bg-blue-600">editar</a>
+                <form action="/category/show/{{$post->id}}" method="POST" >
+                    @csrf
+                    @method("DELETE")
+                    <button type="submit" class="bg-red-500 rounded-md  px-4 py-2 text-gray-100 hover:bg-red-600"> Eliminar post </button>
+                </form>
+            @endif 
+            <a href="/category/" class="bg-blue-500 text-gray-100 px-4 py-2 rounded-md hover:bg-blue-600"> Volver </a> 
+        </div>
     </div>
 </x-app-layout>
