a1b5dba2bbd7959e423d0c5eff1d3602d1260fd6
BrunoFerrada <brunoferrada1212@gmail.com>
2024-06-09 19:27:45 -0300

    editar perfil button added to inicio

--- myBlog/resources/views/dashboard.blade.php
+++ myBlog/resources/views/dashboard.blade.php
@@ -7,9 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white dark:bg-gray-800 dark:text-gray-100 p-6 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class=" text-gray-900 dark:text-gray-100">
                     {{ __("Iniciaste sesión!") }}
+                </div>
+                <div class="pt-6">
+                    <a href="/profile" class=" bg-blue-500 text-gray-100 px-4 py-2 rounded-md hover:bg-blue-600">Editar perfil</a>
                 </div>
             </div>
         </div>
